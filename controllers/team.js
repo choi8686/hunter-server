14aecbd48243af42737bd2074fae5399b996d824
hwanseog choi <this0924@gmail.com>
2019-06-04 12:57:54 +0900

    team get district & store

--- controllers/team.js
+++ controllers/team.js
@@ -26,26 +26,6 @@ router.post("", (req, res) => {
     });
 });
 
-// router.get("", (req, res) => {
-//   models.Team.findAll({
-//     include: [
-//       {
-//         model: models.Location,
-//         attributes: ["district", "store"]
-//       },
-//       {
-//         model: models.Teamimage,
-//         attributes: ["imgUrl"]
-//       }
-//     ]
-//   })
-//     .then(result => {
-//       res.status(200).json(result);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// });
 router.get("/:getStore", async (req, res) => {
   let getStore = req.params.getStore;
 
@@ -71,132 +51,28 @@ router.get("/:getStore", async (req, res) => {
     });
 });
 
-// router.get("/:getDistrict", async (req, res) => {
-//   let getDistrict = req.params.getDistrict;
-//   models.Team.findAll({
-//     include: [
-//       {
-//         model: models.Location,
-//         where: {
-//           district: getDistrict
-//         }
-//       }
-//     ]
-//   })
-//     .then(result => {
-//       res.json(result);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// });
-// router.get("/:district", async (req, res) => {
-//   let getDistrict = await models.Team.findAll({
-//     include: [
-//       {
-//         model: models.Location,
-//         where: { district: req.params.district }
-//       }
-//     ]
-//   });
-//   getDistrict = getDistrict.district;
-//   models.Team.findAll({
-//     include: [
-//       {
-//         model: models.Location,
-//         attributes: ["district"]
-//       }
-//     ]
-//   })
-//     .then(result => {
-//       res.status(200).send(result);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// });
+router.get("/district/:getDistrict", (req, res) => {
+  let getDistrict = req.params.getDistrict;
+  models.Team.findAll({
+    include: [
+      {
+        model: models.Location,
+        where: {
+          district: getDistrict
+        }
+      },
+      {
+        model: models.Teamimage,
+        attributes: ["imgUrl"]
+      }
+    ]
+  })
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
-// router.get("/:store", async (req, res) => {
-//   let getStore = await models.Location.findAll({
-//     where: { id: req.params.id }
-//   });
-//   getStore = getStore.store;
-//   console
-//     .log("======>", getStore)
-//     .then(result => {
-//       res.status(200).send(result);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// });
-
-// models.Team.findAll({
-//   include: [
-//     {
-//       model: models.Location,
-//       store: getStore
-//     }
-//   ]
-// })
-
-// router.get("/:store", (req, res) => {
-//   const { id } = req.params.id;
-//   models.Team.findAll({
-//     include: [
-//       {
-//         model: models.Location,
-//         through: { where: { id: id } },
-//         attributes: ["store"]
-//       }
-//     ]
-//   })
-//     .then(result => {
-//       res.status(200).json(result);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// });
-// [
-//   {
-//     model: Category,
-//     through: { where: { year: 2015 } },
-//     attributes: ["id"]
-//   }
-// ];
-// router.get("", (req, res) => {
-//   models.Team.findAll({
-//     include: [
-//       {
-//         model: models.Location,
-//         attributes: ["district", "store"]
-//       },
-//       {
-//         model: models.Teamimage,
-//         attributes: ["imgUrl"]
-//       }
-//     ]
-//   })
-//     .then(result => {
-//       console.log(result[0].dataValues);
-//       res.status(200).json(result);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// });
-
-// router.get("/:store", (req, res) => {
-//   models.Team.findAll({
-//     model: models.Location,
-//     where: { store: req.params.store }
-//   })
-//     .then(result => {
-//       res.status(200).json(result);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// });
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ var router = express.Router();
 const usersRouter = require("../controllers/users");
 const teamsRouter = require("../controllers/team");
 const likeRouter = require("../controllers/like");
-const districtRouter = require("../controllers/findDistrict");
+// const districtRouter = require("../controllers/findDistrict");
 
 router.use("/users", usersRouter);
 router.use("/teams", teamsRouter);
 router.use("/like", likeRouter);
-router.use("/findDistrict", districtRouter);
+// router.use("/findDistrict", districtRouter);
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
