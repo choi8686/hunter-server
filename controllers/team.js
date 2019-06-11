1646d65f85c349f5918944a68011011c460bef6d
hwanseog choi <this0924@gmail.com>
2019-06-11 16:39:08 +0900

    wrote console.log

--- controllers/team.js
+++ controllers/team.js
@@ -76,27 +76,27 @@ router.get("/district/:getDistrict", (req, res) => {
 
 router.put("/change", async (req, res, next) => {
   const { count, age, comment, teamname, userId, locationId } = req.body;
-  if (count && age && comment && teamname && locationId) {
-    await models.Team.update(
-      {
-        count: count,
-        age: age,
-        comment: comment,
-        teamname: teamname,
-        locationId: locationId
-      },
-      {
-        where: {
-          userId: userId
-        }
+
+  await models.Team.update(
+    {
+      count: count,
+      age: age,
+      comment: comment,
+      teamname: teamname,
+      locationId: locationId
+    },
+    {
+      where: {
+        userId: userId
       }
-    )
-      .then(result => {
-        res.status(200).json(result);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+    }
+  )
+    .then(result => {
+      console.log("팀 수정했다 새끼야");
+      res.status(200).json(result);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 module.exports = router;
